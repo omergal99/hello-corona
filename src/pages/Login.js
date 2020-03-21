dd0698b43b595ed72756913366026c083ce9d42c
omergal99 <omergal99gm@gmail.com>
2020-03-22 00:59:28 +0200

    init

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import './assets/css/App.scss';
 
 function App() {
   return (
     <div className="App">
-      <img src="assets/img/app/corona512.png" alt="" />
     </div>
   );
 }
