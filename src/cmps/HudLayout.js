5f7bf5f0fe599c50a80c183b63b2f4b4a5cac515
omergal99 <omergal99gm@gmail.com>
2020-03-23 21:13:43 +0200

    work on ui-layout

--- src/cmps/HudLayout.js
+++ src/cmps/HudLayout.js
@@ -6,9 +6,17 @@ function HudLayout() {
     <div className="hud-layout">
       <div className="ui-elements">
         <div className="border-left">
-          <div className="circle-end"></div>
+          <div className="dot"></div>
           <div className="strong-line line-1"></div>
           <div className="strong-line line-2"></div>
+        </div>
+        <div className="border-top">
+          <div className="border-top-left">
+            <div className="dot"></div>
+          </div>
+          <div className="border-top-right">
+            <div className="dot"></div>
+          </div>
         </div>
       </div>
     </div>
