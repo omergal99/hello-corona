1b3823ced485e5c82d99fa82a4b721dfc1051e7a
omergal99 <omergal99gm@gmail.com>
2020-03-30 07:05:42 +0300

    Updates

---
+++ precache-manifest.984d62209192427d8949869a6bc6831f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a10bbcb454b90c6874a0ea4e8699ff8",
+    "url": "/hello-corona/index.html"
+  },
+  {
+    "revision": "6cbcbf05524d134d728d",
+    "url": "/hello-corona/static/css/main.442fad32.chunk.css"
+  },
+  {
+    "revision": "72bc7d9624dbfc89e91a",
+    "url": "/hello-corona/static/js/2.eb38f560.chunk.js"
+  },
+  {
+    "revision": "0f2be52aac348a57b5e1722f475cbfd0",
+    "url": "/hello-corona/static/js/2.eb38f560.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cbcbf05524d134d728d",
+    "url": "/hello-corona/static/js/main.6be4e2b1.chunk.js"
+  },
+  {
+    "revision": "f50e6db75dc62d8d2cb5",
+    "url": "/hello-corona/static/js/runtime-main.251d0022.js"
+  }
+]);
