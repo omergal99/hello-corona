395ec12f87dee4cef6934237402f52c4e930fdc1
omergal99 <omergal99gm@gmail.com>
2020-04-16 04:52:01 +0300

    update use button cmp reuse

--- src/cmps/worldDashboard/mapUtils/MapOptions.js
+++ src/cmps/worldDashboard/mapUtils/MapOptions.js
@@ -1,17 +1,38 @@
 import React from "react";
+import Button from '../../elements/Button';
+import { CASES, DEATHS, RECOVERED, TOTAL_TESTS, getDataKeysByKeys } from '../../../constants/DataKeys';
 
-function MapOptions({ isCirclesShow, isAutoFocus,
-  onToggleIsCirclesShow, onToggleIsAutoFocus }) {
+function MapOptions({ isCirclesShow, isAutoFocus, circlesDataKey,
+  onToggleIsCirclesShow, onToggleIsAutoFocus, onSetCirclesDataKey }) {
 
+  const circleClicked = key => {
+    if (circlesDataKey !== key) onSetCirclesDataKey(key);
+  }
+
+  const circlesData = getDataKeysByKeys([CASES, DEATHS, RECOVERED, TOTAL_TESTS]);
+
+  const circlesDataButtons = circlesData.map(circle => {
+    const isActive = circlesDataKey === circle.key;
+    return <Button key={circle.key} text={circle.title} type={`${circle.color} ${isActive && 'active'}`}
+      onClick={() => circleClicked(circle.key)} />
+  })
 
   return (
     <div className="map-options">
-      <button className="toggle-btn" onClick={onToggleIsCirclesShow}>
-        {isCirclesShow ? 'Hide Circles' : 'Show Circles'}
-      </button>
-      <button className="toggle-btn" onClick={onToggleIsAutoFocus}>
-        {isAutoFocus ? 'Dissable Auto Focus' : 'Enable Auto Focus'}
-      </button>
+
+      {isCirclesShow &&
+        <div className="circles-options">
+          {circlesDataButtons}
+        </div>
+      }
+
+      <div className="main-options">
+        <Button text="Hide Circles" toggleText="Show Circles" textCondition={isCirclesShow}
+          onClick={onToggleIsCirclesShow} />
+        <Button text="Dissable Auto Focus" toggleText="Enable Auto Focus" textCondition={isAutoFocus}
+          onClick={onToggleIsAutoFocus} />
+      </div>
+
     </div>
   );
 }
