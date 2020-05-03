d9abfc6d8c8477957f3b983c02eabed7c94f57d6
omergal99 <omergal99gm@gmail.com>
2020-05-03 20:16:00 +0300

    update local storage name

--- src/services/SettingsService.js
+++ src/services/SettingsService.js
@@ -1,28 +1,28 @@
 import StorageService from './StorageService';
 import { INIT_COLORS, PRIMARY_COLOR, SECONDARY_COLOR } from '../constants/CssVariable';
-import { WORLD_MAP_STORAGE_KEY, COLORS_STORAGE_KEY } from '../constants/LocalStorageKeys';
+import { WORLD_MAP, COLORS } from '../constants/LocalStorageKeys';
 
 async function getData() {
   _setCssVariableColors();
   const initState = _getEmpty();
-  const worldMapLocalStorage = StorageService.load(WORLD_MAP_STORAGE_KEY);
+  const worldMapLocalStorage = StorageService.load(WORLD_MAP);
   if (worldMapLocalStorage) initState.worldMap = worldMapLocalStorage;
   return Promise.resolve(initState);
 }
 
 function updateWorldMapLocalStorage({ worldMap }) {
-  StorageService.store(WORLD_MAP_STORAGE_KEY, worldMap);
+  StorageService.store(WORLD_MAP, worldMap);
 }
 
 function updateColorsLocalStorage() {
   const primary = Number(getComputedStyle(document.documentElement).getPropertyValue(PRIMARY_COLOR));
   const secondary = Number(getComputedStyle(document.documentElement).getPropertyValue(SECONDARY_COLOR));
-  StorageService.store(COLORS_STORAGE_KEY, { primary, secondary });
+  StorageService.store(COLORS, { primary, secondary });
 }
 
 function resetColorsLocalStorage() {
   const initColors = INIT_COLORS;
-  StorageService.store(COLORS_STORAGE_KEY, initColors);
+  StorageService.store(COLORS, initColors);
   _setCssVariableColors(initColors);
 }
 
@@ -44,7 +44,7 @@ const _getEmpty = () => ({
 })
 
 const _setCssVariableColors = colors => {
-  const colorsLocalStorage = colors || StorageService.load(COLORS_STORAGE_KEY);
+  const colorsLocalStorage = colors || StorageService.load(COLORS);
   if (!colorsLocalStorage) return;
   document.documentElement.style.setProperty(PRIMARY_COLOR, colorsLocalStorage.primary);
   document.documentElement.style.setProperty(SECONDARY_COLOR, colorsLocalStorage.secondary);
