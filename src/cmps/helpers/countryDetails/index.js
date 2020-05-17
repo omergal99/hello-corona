9c1ec45b5117ddc9c1e8b4974f1ebd82e9542aec
omergal99 <omergal99gm@gmail.com>
2020-05-17 15:15:53 +0300

    fixes

--- src/cmps/helpers/countryDetails/index.js
+++ src/cmps/helpers/countryDetails/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import actions from '../../../store/actions';
+import { useSelector } from 'react-redux';
 
 import CountryDetailsTitle from './CountryDetailsTitle';
 import CountryDetailsList from './CountryDetailsList';
 
 function CountryDetails({ country }) {
-  const dispatch = useDispatch();
+
   const settingsStore = useSelector(state => state.settingsStore);
-
-  const onToggleCOUntryVoice = () => dispatch(actions.toggleIsCountryVoice());
-
+  const sounds = settingsStore ? settingsStore.sounds : { sounds: false };
   return (
     <div className="country-details flex-col">
-      <CountryDetailsTitle country={country} sounds={settingsStore.sounds} />
+      <CountryDetailsTitle country={country} sounds={sounds} />
       <CountryDetailsList country={country} />
     </div>
   );
